6e23f0642fdf6b8cd077de7f89144f10dbba0c62
Murat Demirci <mdemirci01@gmail.com>
2020-12-29 21:06:00 +0300

    customer service hazırlanıyor

--- CustomerVisitPlan.Services/CustomerService.cs
+++ CustomerVisitPlan.Services/CustomerService.cs
@@ -1,8 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CustomerVisitPlan.Data;
+using CustomerVisitPlan.Model;
 
 namespace CustomerVisitPlan.Services
 {
-    public class Class1
+    public class CustomerService : ICustomerService
     {
+        private readonly IRepository<Customer> customerRepository;
+        private readonly IUnitOfWork unitOfWork;
+        public CustomerService(IRepository<Customer> customerRepository, IUnitOfWork unitOfWork)
+        {
+            this.customerRepository = customerRepository;
+            this.unitOfWork = unitOfWork;
+        }
+        public Task<bool> Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer> Get(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Customer> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Insert(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- CustomerVisitPlan.Services/ICustomerService.cs
+++ CustomerVisitPlan.Services/ICustomerService.cs
@@ -1,7 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CustomerVisitPlan.Model;
+
 namespace CustomerVisitPlan.Services
 {
     public interface ICustomerService
     {
+        Task<Customer> Get(string id);
+        IList<Customer> GetAll();
+
+        Task<bool> Insert(Customer entity);
+        Task<bool> Update(Customer entity);
+        Task<bool> Delete(string id);
     }
+
 }
